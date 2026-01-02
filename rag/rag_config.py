769ba4ab1e779e8f448cb0079ccbaaf241466083
KN-P16 <kn@kn.com>
2026-01-02 02:20:31 +0000

    Correct rag mbox read folder

--- rag/rag_config.py
+++ rag/rag_config.py
@@ -10,9 +10,9 @@ import psutil
 
 
 PROJECT_ROOT = Path(__file__).resolve().parent
-RAW_MBOX_DIR = (PROJECT_ROOT / "rag/data/raw/mbox").resolve()
-OUTPUT_JSONL = (PROJECT_ROOT / "rag/data/clean/email_chunks.jsonl").resolve()
-PROCESSED_MBOX_TXT = (PROJECT_ROOT / "rag/data/clean/processed_mboxes.txt").resolve()
+RAW_MBOX_DIR = (PROJECT_ROOT / "data/raw/mbox").resolve()
+OUTPUT_JSONL = (PROJECT_ROOT / "data/clean/email_chunks.jsonl").resolve()
+PROCESSED_MBOX_TXT = (PROJECT_ROOT / "data/clean/processed_mboxes.txt").resolve()
 
 
 @dataclass
