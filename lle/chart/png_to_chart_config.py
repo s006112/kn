3d27aec9d1d1adfd6c45995a31ce1a5366041021
FMV <kn@kn.com>
2026-02-24 04:42:26 +0000

    main.html step

--- lle/chart/png_to_chart_config.py
+++ lle/chart/png_to_chart_config.py
@@ -17,26 +17,26 @@ CONFIG_PATH = RAW_DIR / "chart_config.json"
 FIT_DEFAULT = {"max_degree": 6, "min_degree": 4}
 CHART_DEFAULTS = {
     "lm_test": {
-        "2700": 59.8,
-        "3000": 61.5,
-        "3500": 61.5,
-        "4000": 64.9,
-        "5000": 64.9,
-        "5700": 64.5,
-        "6500": 64.5
+        "2700": 158,
+        "3000": 163,
+        "3500": 168,
+        "4000": 173,
+        "5000": 173,
+        "5700": 173,
+        "6500": 173
     },
-    "CRI": 90,
-    "If_max": 350,
-    "If": 150,
-    "USD": 0.1,
-    "RMB": 0.0365,
-    "Quote_date": "2026-01-28",
+    "CRI": 80,
+    "If_max": 120,
+    "If": 100,
+    "USD": 0.0166,
+    "RMB": 0.131,
+    "Quote_date": "2025-12-22",
     "Vf": 0.0,
     "Tj": 0.0,
-    "FIL": {"domain": {"x_min": 0.0, "x_max": 360.0, "y_min": 0.0, "y_max": 2.0}, "swap_xy": False},
-    "FIV": {"domain": {"x_min": 2.55, "x_max": 3.35, "y_min": 0.0, "y_max": 360.0}, "swap_xy": True},
-    "FTL": {"domain": {"x_min": 0.0, "x_max": 125.0, "y_min": 0.80, "y_max": 1.0}, "swap_xy": False},
-    "FTV": {"domain": {"x_min": 25.0, "x_max": 125.0, "y_min": 0.0, "y_max": 1.0}, "swap_xy": False},
+    "FIL": {"domain": {"x_min": 0.0, "x_max": 120.0, "y_min": 0.0, "y_max": 1.4}, "swap_xy": False},
+    "FIV": {"domain": {"x_min": 6.0, "x_max": 10.0, "y_min": 0.0, "y_max": 120.0}, "swap_xy": True},
+    "FTL": {"domain": {"x_min": 25.0, "x_max": 85.0, "y_min": 0.80, "y_max": 1.0}, "swap_xy": False},
+    "FTV": {"domain": {"x_min": 25.0, "x_max": 85.0, "y_min": 0.90, "y_max": 1.0}, "swap_xy": False},
 }
 SUFFIX_ALIAS = {"FVI": "FIV"}
 CHART_IDS = {"FIL", "FIV", "FTL", "FTV"}
