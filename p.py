5656519154d3e20cd09b427d67866f28cd666b30
P16 <kn@kn.com>
2026-03-09 23:28:10 +0000

    p.py

--- p.py
+++ p.py
@@ -16,15 +16,15 @@ import p_orchestrator  # type: ignore[reportMissingImports]
 # gpt-5.4 $15, gpt-5.2 $14, gpt-5.1 $10, gpt-4.1 $8, gpt-4o, o1 $60, o3 $8,
 # grok-4-1-fast-reasoning $0.5, grok-4-1-fast-non-reasoning $0.5, grok-4-0709 $15
 MODEL_PRETEXT = "gpt-4.1-mini"
+#MODEL_DISTILL = "grok-4-1-fast-reasoning"
 MODEL_DISTILL = "o3"
 MODEL_EXTRACT_MATRIX = {
     "EXTRACT_WATCH_FOLDER": [
-        "gpt-5-mini",
+        #"gpt-5-mini",
         #"sonar-reasoning-pro",
         "grok-4-1-fast-non-reasoning",
         "gemini-3.1-pro-preview",
-        #"gpt-5.4"
-        #"gpt-5.1"
+        "gpt-5.4"
     ],
     "PREMIUM_WATCH_FOLDER": [
         "gpt-5.4",   # gpt-5.2, gpt-5.4
