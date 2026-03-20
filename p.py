dd8a506da47fd109651d4305a5074a81514a5afc
P16 <kn@kn.com>
2026-03-20 06:22:59 +0000

    p.py

--- p.py
+++ p.py
@@ -14,17 +14,17 @@ import p_orchestrator  # type: ignore[reportMissingImports]
 # gemini-2.5-flash $2.5, gemini-2.5-pro $10, gemini-3-flash-preview $3, gemini-3-pro-preview $12, 
 # gpt-5-mini, gpt-5-nano, gpt-4.1-mini, gpt-4.1-nano, gpt-4o-mini, o1-mini, o3-mini, o4-mini,
 # gpt-5.4 $15, gpt-5.2 $14, gpt-5.1 $10, gpt-4.1 $8, gpt-4o, o1 $60, o3 $8,
-# grok-4-1-fast-reasoning $0.5, grok-4-1-fast-non-reasoning $0.5, grok-4-0709 $15
-MODEL_PRETEXT = "gpt-4.1-mini"
+# grok-4-1-fast-reasoning $0.2, grok-4-1-fast-non-reasoning $0.2, grok-4.20-0309-non-reasoning $2.0
+MODEL_PRETEXT = "gpt-5.4-mini"
 #MODEL_DISTILL = "grok-4-1-fast-reasoning"
 MODEL_DISTILL = "o3"
 MODEL_EXTRACT_MATRIX = {
     "EXTRACT_WATCH_FOLDER": [
         #"sonar-reasoning-pro",
-        "grok-4-1-fast-non-reasoning",
+        "grok-4.20-0309-non-reasoning",
         "gemini-3.1-pro-preview",
-        #"gpt-5-mini",
-        "gpt-5.4"
+        "gpt-5.4-mini",
+        #"gpt-5.4"
     ],
     "PREMIUM_WATCH_FOLDER": [
         "gpt-5.4",   # gpt-5.2, gpt-5.4
